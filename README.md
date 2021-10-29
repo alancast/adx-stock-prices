7353f31fb8f1b6d30beda88bb26bad0f853c5608
Alex Lancaster <allancas@microsoft.com>
2021-10-28 18:13:23 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # adx-stock-prices
+
 Upskilling on Azure Data Explorer with tracking stock prices
+
+## Prerequisites
+
+Some packages must be installed in advance.
+Do this by running `pip3 install -r requirements.txt`
+
+## How to run
+
+Run `python3 -m main -h` to see input options
---
+++ main.py
@@ -0,0 +1,29 @@
+# Raw Package
+import argparse
+import time
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+from datetime import datetime as dt
+
+def getStockPrices(tickers: str, sleep_time: int):
+    while True:
+        currentTime = dt.now()
+
+        tickers_arr = tickers.split(',')
+        for ticker in tickers_arr:
+            tkr = yf.Ticker(ticker)
+            print(f"{currentTime}: Ticker {ticker} is currently: {tkr.info['regularMarketPrice']}")
+
+        time.sleep(sleep_time)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = 'Print out stock prices for different stocks')
+    parser.add_argument("-t", "--tickers", default="msft",
+                    help="What tickers do you want to see values for. Separated by commas")
+    parser.add_argument("-s", "--sleep", type=int, default=10,
+                    help="How many seconds to sleep for between iterations")
+    args = parser.parse_args()
+
+    getStockPrices(args.tickers, args.sleep)
